355506a3121567bf32d1f84c9f5b4df97dace7b3
ZakiGll <enzogll2213@gmail.com>
2023-08-02 19:17:02 +0200

    main

---
+++ train.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+# Use the model
+model.train(data="config.yaml", epochs=30)
